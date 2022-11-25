edb7fc01cdce7d5204b4f7fe6e443968379709cf
tadjeddine-berrou <tadjeddine.berrou@gmail.com>
2022-11-25 08:14:02 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'form-builder';
+}
